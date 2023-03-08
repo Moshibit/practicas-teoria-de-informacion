23a824bf92b2b517351fe5f6627996b79469e6ea
Moshibit <39635527+Moshibit@users.noreply.github.com>
2023-03-07 20:34:38 -0600

    Update practica2.py

--- practica2.py
+++ practica2.py
@@ -6,9 +6,9 @@
 
 # from collections.OrderedDict
 # TODO: importar arbol binario y lista o diccionario ordenado
-from collections import OrderedDict
+# from collections import OrderedDict
 # TODO importar parser
-
+# TODO medir mis tiempos
 
 class Empty(Exception):
     """Error attempting to acces an elements from an empty container."""
@@ -146,7 +146,7 @@ class Tree:
 class BinaryTree(Tree):
     """Abstract base class representing a binary tree strcture."""
 
-    # additionals abstract methods
+    # additionals abstract methods --------------------------------------------
     def left(self, p):
         """Retrun a Psition representing p's left child.
         
@@ -154,7 +154,32 @@ class BinaryTree(Tree):
         """
         raise NotImplementedError
 
-    # ...
+    def right(self, p):
+        """Return a Position representing p's right child.
+        
+        Return None if p does not have a right child.
+        """
+        raise NotImplementedError('must be implementes by subclass')
+    
+    # concrete methods implemented in this class 
+    def sibling(self, p):
+        """Return a Position representing p's sibling (or None if no sibling)."""
+        parent = self.parant(p)
+        if parent is None:                  # p must be the root
+            return None                     # root has no sibling
+        else:
+            if p == self.left(parent):
+                return self.right(parent)   # possibly None
+            else:
+                return self.left(parent)    # possibly None
+
+    def children(self, p):
+        """Generate an iteration of Positions representing p's children."""
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p)
+
     def inorder(self):
         """Generate an inorder iteration of positions in the tree."""
         if not self.is_empty():
@@ -176,7 +201,6 @@ class BinaryTree(Tree):
         """Generate an iteration of the tree's positions."""
         return self.inorder()               # make inorder the default
 
-    pass  # TODO completar clase, descomentar el codigo de arriba y borraqr TODO
 
 class LinkedBinaryTree(BinaryTree):
     """Linked representation of a binary tree structure."""
@@ -191,10 +215,155 @@ class LinkedBinaryTree(BinaryTree):
 
     class Position(BinaryTree.Position):
         """An abstract representing the location of a single element"""
-        pass
+        
+        def __init__(self, container, node):
+            """Constructor should not be invoked by user."""
+            self._container = container
+            self._node = node
+        
+        def element(self):
+            """Return the element stored at this Position."""
+            return self._node._element
+        
+        def __eq__(self, other):
+            """Return True if the other is a Position representing the same location."""
+            return type(other) is type(self) and other._node is self._node
+        
+    def _validate(self, p):
+        """Return associated node, if position is valid."""
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p._container is not self:
+            raise ValueError('p does not belong to this container')
+        if p._node._parent is p._node:                   # convention for deprecated nodes
+            raise ValueError('p is no longer valid')
+        return p._node
+    
+    def _make_position(self, node):
+        """Return Position instance for given node (or None if no node)."""
+        return self.Position(self, node) if node is not None else None
+    
+    # binary tree constructor
+    def __init__(self) -> None:
+        """Create an initially empty binary tree."""
+        self._root = None
+        self._size = 0
 
-    pass
-    # TODO completar
+    # public accesors
+    def __len__(self):
+        """Return the root Position of the tree (or None if tree is empty)."""
+        return self._size
+    
+    def root(self):
+        """Return the root Position of the tree (or None if tree is empty)."""
+        return self._make_position
+    
+    def parant(self, p):
+        """Return the Position of p's parent (or None if p is root)."""
+        node = self._validate(p)
+        return self._make_position(node._parent)
+    
+    def left(self, p):
+        """Return the Position of p's left child (or None if no left child)."""
+        node = self._validate(p)
+        return self._make_position(node._left)
+    
+    def right(self, p):
+        """Return the Position of p's right child (or None if no right child)."""
+        node = self._validate(p)
+        return self._make_position(node._right)
+
+    def num_children(self, p):
+        """Return the number of children od Position p."""
+        node = self._validate(p)
+        count = 0
+        if node._left is not None:      # left child exists
+            count += 1
+        if node._right is not None:     # right child exists
+            count += 1
+        return count
+    
+    def _add_root(self, e):
+        """Place elemnt e at the root of an empty tree and return new Position.
+        
+        Raise ValueError if tree nonempty
+        """
+        if self._root is not None: raise ValueError('Root exists')
+        self._size = 1
+        self._root = self._Node(e)
+        return self._make_position(self._root)
+    
+    def _add_left(self, p, e):
+        """Create a new left child for Position p, storing element e
+        
+        Return the Position of new node.
+        Raise ValueError if Position p is invalid or p already has a left child.
+        """
+        node = self._validate(p)
+        if node._left is not None: raise ValueError('Left child exists')
+        self._size += 1
+        node._left = self._Node(e, node)            # node is its parent
+        return self._make_position(node._left)
+    
+    def _add_right(self, p, e):
+        """Create a new right child for Position p, storing element e
+        
+        Return the Position of new node.
+        Raise ValueError if Position p is invalid or p already has a right child.
+        """
+        node = self._validate(p)
+        if node._right is not None: raise ValueError('Right child exists')
+        self._size += 1
+        node._right = self._Node(e, node)            # node is its parent
+        return self._make_position(node._right)
+
+    def _replace(self, p, e):
+        """Replace the element at position p with w, and return old element."""
+        node = self._validate(p)
+        old = node._element
+        node._element = e
+        return old
+    
+    def _delete(self, p):
+        """Delete the node at Position p, and replace it with its child, id any.
+        
+        Return the element that been stored at Position p.
+        Raise ValueError if Position p is invalid or p has two children.
+        """
+        node = self._validate(p)
+        if self.num_children(p) == 2: raise ValueError('p has two children')
+        child = node._left if node._left else node._right      # might be None
+        if child is not None:
+            child._parent = node._parent    # child's grandparent becomes parent
+        if node is self._root:
+            self._root = child              # child becomes root
+        else:
+            parent = node._parent
+            if node is parent._left:
+                parent._left = child
+            else:
+                parent._right = child
+        self._size -= 1
+        node._parent = node                 # convention for deprecated node
+        return node._element
+
+    def _attach(self, p, t1, t2):
+        """Attach trees t1 and t2 left and right subtrees of external p."""
+        node = self._validate(p)
+        if not self.is_leaf(p): raise ValueError('position must be leaf')
+        if not type(self) is type(t1) is type(t2):    # all 3 trees must be same type
+            raise TypeError('Tree types must match')
+        self._size += len(t1) + len(t2)
+        if not t1.is_empty():                   # attaches t1 as left subtree of node
+            t1._root._parent = node
+            node._left = t1._root
+            t1._root = node                     # set t1 instance to empty
+            t1._size = 0
+        if not t2.is_empty():                   # attaches t2 as right subtree of node
+            t2._root._parent = node
+            node._left = t2._root
+            t2._root = node                     # set t2 instance to empty
+            t2._size = 0
 
 
 class LinkedQueue():
@@ -256,21 +425,42 @@ class LinkedQueue():
 
 def preorder_indent(T, p, d):
     """Print preorder representation of subtree of T rooted at p at depth d."""
+    print(2 * d * ' ' + str(p.element()))       # use depth for indentation
+    for c in T.children(p):
+        preorder_indent(T, c, d + 1)            # child depth is d + 1
 
 
 def preorder_label(T, p, d, path):
     """Print labeled representation of subtree of T rooted at p at depth d"""
-    pass
+    label = ' .'.join(str(j + 1) for j in path)     # displayed labels are one-indexed
+    print(2 * d * ' ' + label, p.element())
+    path.append(0)                                  # path entires are zero-indexed
+    for c in T.children(p):
+        preorder_label(T, c, d + 1, path)           # child depth is d + 1
+        path[-1] += 1
+    path.pop()
 
 
 def parenthsize(T, p):
     """Print parenthesized representation of subtree of T rooted at p"""
-    pass
+    print(p.element(), end='')      # use of end avoids trailling newline
+    if not T.is_leaf(p):
+        first_time = True
+        for c in T.children(p):
+            sep = ' (' if first_time else ', '  # any future passes will not be the first
+            print(sep, end='')
+            first_time = False              # any future passes will not be the first
+            parenthsize(T, c)               # recur on child
+        print(')', end='')                  # include closing parenthesis
 
 
 def disk_space(T, p):
     """Return total disk space for subtree of T rooted at p."""
-    pass
+    subtotal = p.element().space()      # space used at position p
+    for c in T.children(p):
+        subtotal += disk_space(T, c)    # add child's space to subtotal
+    return subtotal
+
 
 
 # ###############################################
@@ -358,6 +548,7 @@ def main():
 
 
 if __name__ == "__main__":
-    print("Processing...")
-    main()
-    print("Done.")
+    # print("Processing...")
+    # main()
+    # print("Done.")
+    T = LinkedBinaryTree()
