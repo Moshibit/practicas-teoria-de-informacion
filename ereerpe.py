2935fcc60891d22291ac00374d8c6f47959de593
Moshibit <39635527+Moshibit@users.noreply.github.com>
2023-03-21 17:00:06 -0600

    Update ereerpe.py

--- ereerpe.py
+++ ereerpe.py
@@ -14,36 +14,69 @@ def find_middle(lst):
       return p if abs(s - k) < abs(s - b) else j
   return
 
-def shannon(iterable):
-  print("***")
-  rv = None
-  if len(iterable) == 1: return rv
-  half = round(sum(iterable) / 2)
-  print("h:", half)
-  print("I: ", iterable)
-  group1=group2=0
-  diff1=diff2=diff3=0
-  for index in range(len(iterable)-2):
-    group1 = iterable[:index+1]
-    group2 = iterable[:index+2]
-    sum1 = sum(group1)
-    sum2 = sum(group2)
-    diff1 = (sum1 - half)
-    diff2 = (sum2 - half)
-    print("g:", group1, group2)
-    print(sum1, sum2)
-    print("d:", diff1, diff2, diff3)
-    print("-------------------")
-    if sum1 >= half:
-      break
+class shannon():
+  def __init__(self, d):
+    self.d = d
+    self.code = {}
+
+  def shanon_start(self):
+    print("***")
+    iterable=list(self.d.values())
+    rv = None
+    if len(iterable) == 1: return rv
+    half = round(sum(iterable) / 2)
+    print("h:", half)
+    print("I: ", iterable)
+    group1=group2=0
+    diff1=diff2=diff3=0
+    for index in range(len(iterable)-2):
+      group1 = iterable[:index+1]
+      group2 = iterable[:index+2]
+      sum1 = sum(group1)
+      sum2 = sum(group2)
+      diff1 = (sum1 - half)
+      diff2 = (sum2 - half)
+      print("g:", group1, group2)
+      print(sum1, sum2)
+      print("d:", diff1, diff2, diff3)
+      print("-------------------")
+      if sum1 >= half:
+        break
+      rv = (group1, group2)
+      if rv:
+        self.sha(group1, "0")
+        self.sha(group2, "1")
+
+  def sha(self, iterable, c):
+    print("*_*")
+    rv = None
+    if len(iterable) == 1: return rv
+    half = round(sum(iterable) / 2)
+    print("h:", half)
+    print("I: ", iterable)
+    group1=group2=0
+    diff1=diff2=diff3=0
+    for index in range(len(iterable)-2):
+      group1 = iterable[:index+1]
+      group2 = iterable[:index+2]
+      sum1 = sum(group1)
+      sum2 = sum(group2)
+      diff1 = (sum1 - half)
+      diff2 = (sum2 - half)
+      print("g:", group1, group2)
+      print(sum1, sum2)
+      print("d:", diff1, diff2, diff3)
+      print("-------------------")
+      if sum1 >= half:
+        break
 
     rv = (group1, group2)
-  if rv:
-    cero=shannon(group1)
-    uno=shannon(group2)
-    print()
-
-  return rv
+    if rv:
+      sha(group1, c+"0")
+      sha(group2, c+"1")
+    else:
+      print(self.code)
+    return rv
     
 
 
@@ -53,6 +86,10 @@ print('Hello, world!')
 l = [1/2, 1/4, 1/8, 1/16, 1/32, 1/64, 1/64]
 anita = [6/15, 2/15, 2/15, 2/15, 2/15, 1/15]
 anita2 = [6, 2, 2, 2, 2, 1]
+d = {"a":6, "i":2, "l":2, "n":3, "t":2, "v":1}
 
 #print(sum(anita2))
-print(shannon(anita2))
+#print(shannon(anita2))
+w = shannon(d)
+w.shanon_start()
+print(w.code)
