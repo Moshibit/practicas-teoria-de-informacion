d86c14fa8e8caf19b72dc50869bd52aff1c26584
Moshibit <39635527+Moshibit@users.noreply.github.com>
2023-06-05 19:23:44 -0600

    Create main.py

---
+++ practica03/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+Título: Práctica 3 - XXXXXXX.
+Descripción: 
+Entrada: 
+Salida: 
+Autores: 
+Fecha de creación: 05/06/2023
+Última fecha de edición: 
+
+"""
+
+# FUNCIÓN MAIN DEL PROYECTO
+# -------------------------
+
+def main():
+    """ Main """
+    print("Start")
+
+    # TODO: agregar la lógica
+
+    print("End")
+
+if __name__ == "__main__":
+    main()
