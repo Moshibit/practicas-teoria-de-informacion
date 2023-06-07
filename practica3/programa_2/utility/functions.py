903dd169ace23ffb3534210bf06b21fdaab77600
Moshibit <39635527+Moshibit@users.noreply.github.com>
2023-06-06 21:09:54 -0600

    Add files via upload

---
+++ practica3/programa_2/utility/functions.py
@@ -0,0 +1,15 @@
+"""
+Módulo que contiene funciones útiles para el proyecto.
+"""
+
+def detect_errors (info_hex, polynomial, init_vector):
+    """
+    calcula el CRC decuardo a los parametros enviados.
+    """
+    # Código para calcular CRC
+    # ...
+
+    # DEBUG:
+    print(info_hex, polynomial, init_vector)
+    print("Hola_mundo")
+    return ("Hola mundo")
