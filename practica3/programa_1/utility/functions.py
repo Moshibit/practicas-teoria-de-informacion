44cc9daae337a72e62b4f679158ba005cfb92423
Moshibit <39635527+Moshibit@users.noreply.github.com>
2023-06-06 22:04:39 -0600

    Update functions.py

--- practica3/programa_1/utility/functions.py
+++ practica3/programa_1/utility/functions.py
@@ -4,12 +4,14 @@ Módulo que contiene funciones útiles para el proyecto.
 
 def calculate_crc(text, crc_type, vector):
     """
-    calcula el CRC decuardo a los parametros enviados.
+    Calcula el CRC decuardo a los parametros enviados.
     """
     # Código para calcular CRC
     # ...
 
-    # DEBUG:
-    print(text, crc_type, vector)
-    print("Hola_mundo")
-    return ("Hola", "mundo")
+    # # DEBUG:
+    # print(info_hex, polynomial, init_vector)
+    # print("Hola_mundo")
+    # # DEBUG: Fin
+
+    return "Hola mundo" # TODO cambiar por el verdadero retorno
