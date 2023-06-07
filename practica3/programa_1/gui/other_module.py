f3f058e32aded73a9c4d4e41898c23b45fa201c9
Moshibit <39635527+Moshibit@users.noreply.github.com>
2023-06-06 21:17:58 -0600

    Delete other_module.py

--- practica3/programa_1/gui/other_module.py
+++
@@ -1,9 +0,0 @@
-"""
-Módulo que contiene otras clases o módulos relacionados con la interfaz gráfica de usuario.
-Aquí puedes agregar más código según sea necesario.
-"""
-# Importar los módulos o clases adicionales requeridos
-# ...
-
-# Definir más clases o módulos relacionados con la GUI
-# ...
