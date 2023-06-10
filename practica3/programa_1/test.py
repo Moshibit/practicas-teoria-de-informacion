c58e8cb54a1965a7385346447753e7d61a9a0694
Moshibit <39635527+Moshibit@users.noreply.github.com>
2023-06-10 17:25:06 -0600

    Add files via upload

---
+++ practica3/programa_1/test.py
@@ -0,0 +1,60 @@
+"""placeholder"""
+g = "110100111"
+c = "0" * (len(g) - 1)
+d = "101"
+
+
+def crc_encode(g, c, d):
+    """placeholder"""
+    g = [ int(x, 2) for x in g ]
+    c = [ int(x, 2) for x in c ]
+    d = [ int(x, 2) for x in d ]
+    
+    print("-"*40)
+    for b in d:
+        p = c[0] ^ b
+        for i in range(0, len(c) - 1):
+            # print(c[i], " = ", c[i+1], " ^ (" , p, " & ", g[i+1], " )" )
+            c[i] = c[i+1] ^ (p & g[i+1])
+        c[-1] = p
+        print(b, p, c[::-1])
+
+    print("-"*40)
+    c = c[::-1]
+    print(c)
+    return d + c
+
+
+def crc_decode(g, c, m):
+    """placeholder"""
+    g = [int(x, 2) for x in g ]
+    c = [int(x, 2) for x in c ]
+    #m = [int(x, 2) for x in m]
+    
+    print("-"*40)
+    for b in m:
+        p = c[0] ^ b
+        for i in range(0, len(c) - 1):
+            # print(c[i], " = ", c[i+1], " ^ (" , p, " & ", g[i+1], " )" )
+            c[i] = c[i+1] ^ (p & g[i+1])
+        c[-1] = p
+        print(b, p, c[::-1])
+
+    print("-"*40)
+    print(c)
+    return any(c)
+
+
+
+def main():
+    """main"""
+    m = crc_encode(g,c,d)
+    print(m)
+    r = crc_decode(g,c,m)
+    print(r)
+
+
+if __name__ == "__main__":
+    print("start")
+    main()
+    print("end")
