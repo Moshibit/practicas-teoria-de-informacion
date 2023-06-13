fe80761d0c53d7ccccf3f9519a179e3e868a7c50
Moshibit <39635527+Moshibit@users.noreply.github.com>
2023-06-13 12:53:53 -0600

    Update main_window.py

--- practica3/programa_2/gui/main_window.py
+++ practica3/programa_2/gui/main_window.py
@@ -5,6 +5,7 @@ aplicación.
 import re
 
 from tkinter import Frame, Button, Label, Entry, StringVar, Tk
+from tkinter import Radiobutton, Event
 from utility.functions import detect_errors
 
 class MainWindow(Frame):
@@ -24,6 +25,8 @@ class MainWindow(Frame):
         self.info = StringVar()
         self.polynomial = StringVar()
         self.init_vector = StringVar()
+        self.radio_var_format = StringVar()
+        self.radio_var_format.set("binary")  # Valor por defecto
         self.pack()
         self.create_widgets()
 
@@ -34,6 +37,9 @@ class MainWindow(Frame):
         label_info = Label(self, text="Información en hexadecimal:")
         label_info.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
+        label_polynomial_format = Label(self, text="Formato del polinomio:")
+        label_polynomial_format.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+
         entry_info = Entry(self,
             width=30,
             textvariable=self.info,
@@ -42,32 +48,49 @@ class MainWindow(Frame):
             )
         entry_info.grid(row=0, column=1, padx=10, pady=10)
 
-        label_polynomial = Label(self, text="Polinomio:")
-        label_polynomial.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        radio_binary = Radiobutton(self,
+            text="Binary",
+            variable=self.radio_var_format,
+            value="binary",
+            command=self.delete_entry_polynomial,
+            )
+        radio_hexadecimal = Radiobutton(self,
+            text="Hexadecimal",
+            variable=self.radio_var_format,
+            value="hexadecimal",
+            command=self.delete_entry_polynomial,
+            )
 
-        entry_polynomial = Entry(self, width=30,
+        radio_binary.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+        radio_hexadecimal.grid(row=1, column=2, padx=10, pady=10, sticky="w")
+
+        label_polynomial = Label(self, text="Polinomio:")
+        label_polynomial.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+
+        self.entry_polynomial = Entry(self, width=30,
             textvariable=self.polynomial,
             validate="key",
             validatecommand=(self.register(self.validate_polinomial), "%P"),
             )
-        entry_polynomial.grid(row=1, column=1, padx=10, pady=10)
+        self.entry_polynomial.bind("<FocusIn>", self.update_polynomial)
+        self.entry_polynomial.grid(row=2, column=1, padx=10, pady=10)
 
         label_init_vector = Label(self, text="Vector de inicialización:")
-        label_init_vector.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        label_init_vector.grid(row=3, column=0, padx=10, pady=10, sticky="w")
 
-        entry_init_vector = Entry(self,
+        self.entry_init_vector = Entry(self,
             width=30,
             textvariable=self.init_vector,
-            # validate="key",
-            # validatecommand=(self.register(self.validate_init_vector), "%P"),
+            validate="key",
+            validatecommand=(self.register(self.validate_init_vector), "%P"),
             )
-        entry_init_vector.grid(row=2, column=1, padx=10, pady=10)
+        self.entry_init_vector.grid(row=3, column=1, padx=10, pady=10)
 
         button_check = Button(self, text="Verificar", command=self.detect_errors)
-        button_check.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+        button_check.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
         self.label_result = Label(self, text="Resultado:")
-        self.label_result.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+        self.label_result.grid(row=5, column=0, padx=10, pady=10, sticky="w")
 
     def validate_hex_text(self, new_value: str) -> bool:
         """Valida la entrada de información en formato hexadecimal"""
@@ -76,35 +99,53 @@ class MainWindow(Frame):
             return True
 
         return False
-
+        
+    def delete_entry_polynomial(self):
+        """Limpia el campo del polinomio"""
+        self.entry_polynomial.delete(0, 'end')
+        self.entry_init_vector.delete(0, 'end')
 
     def validate_polinomial(self, new_value: str) -> bool:
         """Valida el polinomio CRC"""
-        # Verifica que el polinomio esté en formato binario o hexadecimal
-        if re.match(r"^[01]*$", new_value) or re.match(r"^[0-9a-fA-F]*$", new_value):
-            return True
+        if self.radio_var_format.get() == "binary":
+            # Validación en formato binario
+            if re.match(r"^[01]*$", new_value):
+                return True
+        elif self.radio_var_format.get() == "hexadecimal":
+            # Validación en formato hexadecimal
+            if re.match(r"^[0-9a-fA-F]*$", new_value):
+                return True
 
         return False
+    
+    def update_polynomial(self, event=None) -> None:
+        """Actualiza el campo del polinomio y borra el campo del vector de inicialización"""
+        self.entry_init_vector.delete(0, "end")
 
-    # NOTE: Halgo no está bien. En ciertas circunstancias un código en binario
-    # pude ser considerado hexadeciamal y viceversa. Tambien causa problemas
-    # si el campo de del polinomio está vacio.
-    # def validate_init_vector(self, new_value: str) -> bool:
-    #     """Valida el vector de inicialización"""
-    #     # Verifica que el vector esté en formato binario o hexadecimal
-    #     if re.match(r"^[01]*$", new_value):
-    #         # Converte el polinomio CRC a binario para obtener su longitud
-    #         crc_polynomial = self.polynomial.get()
-    #         if re.match(r"^[0-9a-fA-F]*$", crc_polynomial):
-    #             crc_binary = bin(int(crc_polynomial, 16))[2:]
-    #             if len(new_value) <= len(crc_binary):
-    #                 return True
-    #         else:
-    #             crc_binary = crc_polynomial
-    #             if len(new_value) == len(crc_binary):
-    #                 return True
+    # NOTE: causa problemas si el campo de del polinomio está vacio.
+    def validate_init_vector(self, new_value: str) -> bool:
+        """Valida el vector de inicialización"""
+        radio_var_format = self.radio_var_format.get()
+        
+        if re.match(r"^[01]*$", new_value):
+            crc_polynomial = self.polynomial.get()
+            
+            if radio_var_format == "hexadecimal":
+                # Bloquear escritura si el campo del polinomio está vacío
+                if not crc_polynomial:
+                    return False
+                crc_binary = bin(int(crc_polynomial, 16))[2:]
+                if len(new_value) <= len(crc_binary) - 1:
+                    return True
+            elif radio_var_format == "binary":
+                # Bloquear escritura si el campo del polinomio está vacío
+                if not crc_polynomial:
+                    return False
+                crc_binary = crc_polynomial
+                if len(new_value) <= len(crc_binary) - 1:
+                    return True
 
-    #     return False
+        return False
 
 
 
